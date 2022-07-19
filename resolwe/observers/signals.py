0cc183ec70f46ff2d4f5528c09f29b836d803a91
Yon Ploj <yon.ploj@genialis.com>
2022-07-19 16:12:31 +0200

    WIP: detect changes in PermissionModel

--- resolwe/observers/signals.py
+++ resolwe/observers/signals.py
@@ -82,70 +82,75 @@ def model_pre_save(sender, instance, created=False, **kwargs):
     global IN_MIGRATIONS
     if IN_MIGRATIONS:
         return
-    instance._was_adding = instance._state.adding
 
     # created is always False. Use instance._state.adding
     if instance._state.adding:
         return
+        if isinstance(instance, PermissionModel):
+            instance = instance.permission_group
+        else:
+            return
 
     if isinstance(instance, PermissionObject):
-        old_permissions = dict()
-        new_permissions = dict()
-        for observer in Observer.objects.all():
-            new_permissions[
-                observer.user.pk
-            ] = instance.permission_group.get_permission(observer.user)
-
-            saved_instance = type(instance).objects.get(pk=instance.pk)
-            old_permissions[
-                observer.user.pk
-            ] = saved_instance.permission_group.get_permission(observer.user)
+        # or isinstance(instance, PermissionModel):
+        saved_instance = type(instance).objects.get(pk=instance.pk)
+        old_perm_group = instance.permission_group
+        new_perm_group = saved_instance.permission_group
 
         # Calculate who gained and lost permissions to the object.
-        gains, losses = permission_diff(old_permissions, new_permissions)
+        gains, losses = permission_diff(old_perm_group, new_perm_group)
 
-        channel_layer = get_channel_layer()
-        for change_type, user_ids in (
-            (CHANGE_TYPE_CREATE, gains),
-            (CHANGE_TYPE_DELETE, losses),
-        ):
-            message = {
-                "type": TYPE_ITEM_UPDATE,
-                "table": instance._meta.db_table,
-                "type_of_change": change_type,
-                "primary_key": str(instance.pk),
-                "app_label": instance._meta.app_label,
-                "model_name": instance._meta.object_name,
-            }
-            session_ids = list(
-                Observer.objects.filter(
-                    user__in=user_ids,
-                    table=instance._meta.db_table,
-                    resource_pk=instance.pk,
-                )
-                .values_list("session_id", flat=True)
-                .distinct()
-            )
-            for session_id in session_ids:
-                channel = GROUP_SESSIONS.format(session_id=session_id)
+        print(
+            "pre-save on",
+            instance,
+            "with created =",
+            instance._state.adding,
+            "has (gains,losses) = ",
+            (gains, losses),
+        )
 
-                transaction.on_commit(
-                    lambda: async_to_sync(channel_layer.send)(channel, message)
-                )
+        # Announce to relevant observers
+        announce_permission_changes(instance, gains, losses)
 
-        # Delete irrelevant observers.
+        # Delete redundant observers.
         Observer.objects.filter(
             user__in=losses, table=instance._meta.db_table, resource_pk=instance.pk
         ).delete()
+    elif isinstance(instance, PermissionGroup):
+        saved_instance = type(instance).objects.get(pk=instance.pk)
+
+        # Calculate who gained and lost permissions to the object.
+        gains, losses = permission_diff(instance, saved_instance)
+
+        print(
+            "pre-save on",
+            instance,
+            "->",
+            saved_instance,
+            "with created =",
+            instance._state.adding,
+            "has (gains,losses) = ",
+            (gains, losses),
+        )
+
+        # Announce to relevant observers
+        announce_permission_changes(instance, gains, losses)
 
 
-def permission_diff(old_permissions, new_permissions):
-    """Calculate the difference in permissions.
+def permission_diff(old_perm_group, new_perm_group):
+    """Calculate the difference in permissions given two PermissionGroups.
 
-    Accept two dicts of the form {user_id: permission} and return two lists.
-    The first is user_ids of users who gained permissions (0 to >0), and the
-    second is user_ids of users who lost permissions (>0 to 0)
+    Accept two PermissionGroup objects and return two lists. The first list is
+    user_ids of users who gained permissions (0 to >0), and the second list is
+    user_ids of users who lost permissions (>0 to 0).
     """
+    old_permissions = dict()
+    new_permissions = dict()
+    for observer in Observer.objects.all():
+        new_permissions[observer.user.pk] = old_perm_group.get_permission(observer.user)
+        old_permissions[observer.user.pk] = new_perm_group.get_permission(observer.user)
+
+    # Calculate the list of people who lost/gained permissions
     gains = []
     losses = []
     # Keys in both readings may not match in an edge case where a subscription
@@ -157,3 +162,40 @@ def permission_diff(old_permissions, new_permissions):
         if old_permissions[key] > 0 and new_permissions[key] == 0:
             losses.append(key)
     return (gains, losses)
+
+
+def announce_permission_changes(instance, gains, losses):
+    """Register on_commit calls to notify observers that permissions changed.
+
+    Given an instance and an array of user_ids who gained/lost permissions for
+    it, all relevant observers will be notified of instance creation/deletion.
+    """
+    channel_layer = get_channel_layer()
+    for change_type, user_ids in (
+        (CHANGE_TYPE_CREATE, gains),
+        (CHANGE_TYPE_DELETE, losses),
+    ):
+
+        message = {
+            "type": TYPE_ITEM_UPDATE,
+            "table": instance._meta.db_table,
+            "type_of_change": change_type,
+            "primary_key": str(instance.pk),
+            "app_label": instance._meta.app_label,
+            "model_name": instance._meta.object_name,
+        }
+        session_ids = list(
+            Observer.objects.filter(
+                user__in=user_ids,
+                table=instance._meta.db_table,
+                resource_pk=instance.pk,
+            )
+            .values_list("session_id", flat=True)
+            .distinct()
+        )
+        for session_id in session_ids:
+            channel = GROUP_SESSIONS.format(session_id=session_id)
+
+            transaction.on_commit(
+                lambda: async_to_sync(channel_layer.send)(channel, message)
+            )
