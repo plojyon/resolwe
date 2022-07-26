a721f1ae65abf2d3eaa7c5f1aac854cea4955372
Yon Ploj <yon.ploj@genialis.com>
2022-07-26 12:27:12 +0200

    Add unsubscribe feature

--- resolwe/observers/mixins.py
+++ resolwe/observers/mixins.py
@@ -5,7 +5,7 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 
 from resolwe.flow.models import Data, DescriptorSchema, Process
-
+from rest_framework import status
 from .models import Observer
 
 
@@ -15,47 +15,40 @@ class ObservableMixin:
 
         if pk is not None:
             if not model.objects.filter_for_user(request.user).filter(pk=pk).exists():
-                return Response({"error": "Item does not exist"}, status=400)
+                resp = {"error": "Item does not exist"}
+                return Response(resp, status=status.HTTP_400_BAD_REQUEST)
 
+        subscription_ids = []
         for change_type in change_types:
-            Observer.objects.get_or_create(
+            observer, _ = Observer.objects.get_or_create(
                 table=model._meta.db_table,
                 resource_pk=pk,
                 change_type=change_type,
                 session_id=request.query_params.get("session_id"),
                 user=request.user,
             )
-        return Response(f"Thank u for subscribing to {model.__name__} c:")
+            subscription_ids.append(observer.subscription_id)
+        return Response(str(subscription_ids))
 
-    # TODO: require auth, disable GET
     @action(detail=True, methods=["post"], url_path="subscribe")
     def subscribe_detail(self, request, pk=None):
         return self.subscribe(request, pk=pk, change_types=("UPDATE", "DELETE"))
 
-    # TODO: require auth, disable GET
     @action(detail=False, methods=["post"], url_path="subscribe")
     def subscribe_list(self, request):
         return self.subscribe(request, change_types=("CREATE", "DELETE"))
 
-    # def unsubscribe(self, request, pk=None):
-    #     pass
+    def unsubscribe(self, subscription_id=None):
+        if subscription_id is None:
+            resp = {"error": "Missing subscription_id"}
+            return Response(resp, status=status.HTTP_400_BAD_REQUEST)
+        Observer.objects.filter(subscription_id=subscription_id).delete()
+        return Response()
 
-    # @action(detail=False, methods=["get"])
-    # def make_data(self, request, pk=None):
-    #     user = get_user_model().objects.create(
-    #         username="bobber3",
-    #         email="user@test.com",
-    #         first_name="John",
-    #         last_name="Williams",
-    #     )
-    #     process = Process.objects.create(name="Dummy process", contributor=user)
-    #     data = Data.objects.create(
-    #         pk=1,
-    #         name="Public data",
-    #         slug="public-data2",
-    #         contributor=user,
-    #         process=process,
-    #         size=0,
-    #     )
-    #     serializer = self.get_serializer(data)
-    #     return Response(serializer.data)
+    @action(detail=True, methods=["post"], url_path="unsubscribe")
+    def unsubscribe_detail(self, request, pk=None):
+        return self.unsubscribe(request.query_params.get("subscription_id", None))
+
+    @action(detail=False, methods=["post"], url_path="unsubscribe")
+    def unsubscribe_list(self, request):
+        return self.unsubscribe(request.query_params.get("subscription_id", None))
