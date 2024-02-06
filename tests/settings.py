7cc2e35c86e7a8604583a8c1d9d06f187ab0e3a8
Yon Ploj <yon.ploj@genialis.com>
2024-02-06 09:42:47 +0100

    Set docker18-2 docker18-2

--- tests/settings.py
+++ tests/settings.py
@@ -108,7 +108,7 @@ REDIS_CONNECTION_STRING = "{protocol}://{host}:{port}/{db}".format(**REDIS_CONNE
 LISTENER_CONNECTION = {
     # Keys in the hosts dictionary are workload connector names. Currently
     # supported are 'local', 'kubertenes', 'celery' and 'slurm'.
-    "hosts": {"local": "172.18.0.1"},
+    "hosts": {"local": "172.18.0.2"},
     "port": int(os.environ.get("RESOLWE_LISTENER_SERVICE_PORT", 53893)),
     "min_port": 50000,
     "max_port": 50050,
